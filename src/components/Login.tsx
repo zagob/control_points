ff8201c27b588a56b480f4f6514e3d5689e769a8
Matheus <matheusus1_23@hotmail.com>
2022-05-27 13:55:02 -0300

    ajust layout login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,14 @@
-import { Box, Button, Flex, Heading, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  HStack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { IoMdClock } from "react-icons/io";
 import { InputTime } from "../components/InputTime";
 import { FcGoogle } from "react-icons/fc";
@@ -14,30 +24,65 @@ export function Login() {
   return (
     <Flex w="100%" alignItems="center" justify="center" height="100vh">
       <Flex gap="10px" flexDirection="column" alignItems="center">
+        <Flex width="100%">
+          <Flex
+            flexDirection={{ base: "column", lg: "column" }}
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Heading color="#fff" fontSize={{ base: "18px", lg: "28px" }}>
+              CONTROLE DE FORMA FÁCIL
+            </Heading>
+            <Heading color="#fff" fontSize={{ base: "18px", lg: "26px" }}>
+              SEUS PONTOS DE HORÁRIOS.
+            </Heading>
+          </Flex>
+          <IoMdClock fontSize="120px" color="#fff" />
+        </Flex>
         <Flex
-          w={{ base: '', lg: '100%' }}
+          w={{ base: "", lg: "100%" }}
           gap="12px"
           justifyContent="space-around"
-          flexDirection={{ base: "column", lg: "row" }}
+          flexDirection="column"
         >
-          <InputTime />
-          <InputTime />
-          <InputTime />
-          <InputTime />
+          <HStack>
+            <FormControl>
+              <FormLabel color="#fff">Entrada 1</FormLabel>
+              <InputTime />
+            </FormControl>
+            <FormControl>
+              <FormLabel color="#fff">Saída 1</FormLabel>
+              <InputTime />
+            </FormControl>
+          </HStack>
+          <HStack>
+            <FormControl>
+              <FormLabel color="#fff">Entrada 2</FormLabel>
+              <InputTime />
+            </FormControl>
+            <FormControl>
+              <FormLabel color="#fff">Saída 2</FormLabel>
+              <InputTime />
+            </FormControl>
+          </HStack>
+          <Button
+            textTransform="uppercase"
+            padding={{ base: "10px 0", lg: "32px 0" }}
+            fontSize={{ base: "18", lg: "26px" }}
+            fontWeight="700"
+            background="green.400"
+            w="100%"
+            _hover={{ opacity: "0.8" }}
+          >
+            Simular
+          </Button>
         </Flex>
-        <Button
-          textTransform="uppercase"
-          padding={{ base: "10px 0", lg: "32px 0" }}
-          fontSize={{ base: "18", lg: "26px" }}
-          fontWeight="700"
-          background="green.400"
-          w={{ base: "200px", lg: "100%" }}
-          _hover={{ opacity: "0.8" }}
-        >
-          Simular
-        </Button>
         <Box w="full" mt="32px">
-          <Text fontSize={{ base: '18px', lg: '22px' }} color="#fff" fontWeight="300">
+          <Text
+            fontSize={{ base: "18px", lg: "22px" }}
+            color="#fff"
+            fontWeight="300"
+          >
             Faça login com google para gerenciar <br /> suas horas de pontos.
           </Text>
 
